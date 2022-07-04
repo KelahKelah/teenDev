a81fc748358fbb8102b0cad8d0e44ffaf5edc21b
KelahKelah <kelechiekebor@gmail.com>
2022-07-04 08:04:16 +0100

    cliackable button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const handleNextPage = () => {
-    history.pushState(null, "./download.html")
+    history.pushState(null, "" , "./download.html")
+    location.reload()
+    return false
 }
 const buttonElement = document.getElementById("banner-button")
+console.log(buttonElement)
 buttonElement.addEventListener("click", handleNextPage)
